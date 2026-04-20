30bcce1985ce1a941184d8ac8169f82477a30f8c
Kerstro <castrosituma@hotmail.com>
2026-04-20 04:39:04 -0500

    Added Even java file

---
+++ ArrayLesson.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArrayLesson {
+    public static void main(String[] args) {
+        int[] List = new int[4];
+        List[0] = 4;
+        List[1] = 8;
+        List[2] = 6;
+        Arrays.toString(List);
+        System.out.println(Arrays.toString(List));
+    }
+}
---
+++ Even.java
@@ -0,0 +1,10 @@
+public class Even {
+    // public Even(){}
+    public static void main(String[] args) {
+        int x = 1;
+        while (x <= 100) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
