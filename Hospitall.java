6c4f7bd199d5d3252d6dcfedfdfce28f8a1b6a87
Kerstro <castrosituma@hotmail.com>
2026-05-15 08:58:45 -0500

    Redesigned the code to use OOP principles

--- Hospitall.java
+++ Hospitall.java
@@ -1,22 +1,50 @@
 import javax.swing.JOptionPane;
 
 public class Hospitall {
+    float pressure;
+    float temp;
 
-    public static void main(String[] args) {
-
+    void getinput() {
         String input1 = JOptionPane.showInputDialog(null, "Enter pressure of the patient: ");
-        float pressure = Float.parseFloat(input1);
+        pressure = Float.parseFloat(input1);
+        // return pressure;
 
         String input2 = JOptionPane.showInputDialog(null, "Enter the temperature of the patient: ");
-        float temp = Float.parseFloat(input2);
+        temp = Float.parseFloat(input2);
+        // return temp;
+    }
 
+    void diagnosePatient() {
         if (pressure > 140 && temp > 38) {
             JOptionPane.showMessageDialog(null, "\"Critical ADMIT🏥 patient\"");
         } else if (pressure < 140) {
             JOptionPane.showMessageDialog(null, "\"Give MEDICATION 💉 to the patient\"");
         } else if (pressure > 140 && temp < 38) {
-            JOptionPane.showMessageDialog(null, "Send patient to the 🧪LABARATORY");
+            JOptionPane.showMessageDialog(null, "Send patient to the LABARATORY🥼");
         }
+    }
+
+    public static void main(String[] args) {
+
+        Hospitall patient = new Hospitall();
+        patient.getinput();
+        patient.diagnosePatient();
+
+        // String input1 = JOptionPane.showInputDialog(null, "Enter pressure of the
+        // patient: ");
+        // float pressure = Float.parseFloat(input1);
+
+        // String input2 = JOptionPane.showInputDialog(null, "Enter the temperature of
+        // the patient: ");
+        // float temp = Float.parseFloat(input2);
+
+        // if (pressure > 140 && temp > 38) {
+        // JOptionPane.showMessageDialog(null, "\"Critical ADMIT🏥 patient\"");
+        // } else if (pressure < 140) {
+        // JOptionPane.showMessageDialog(null, "\"Give MEDICATION 💉 to the patient\"");
+        // } else if (pressure > 140 && temp < 38) {
+        // JOptionPane.showMessageDialog(null, "Send patient to the LABARATORY🥼");
+        // }
 
     }
 }
